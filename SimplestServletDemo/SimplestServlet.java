482cef75daf7891fa8457a44916a08808189f0c1
phuongtm1911 <50118891+phuongtm1911@users.noreply.github.com>
2019-05-03 20:59:01 -0500

    Create SimplestServlet.java

---
+++ SimplestServletDemo/SimplestServlet.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mum.cs472;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 987132
+ */
+public class SimplestServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<form method='post'>");
+        out.print("<p>Please click the button</p>");
+        out.print("<input type='submit' value='Click me'/>");
+        out.print("</form>");
+        out.print("</body></html>");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.print("<html><head><title>Test</title></head><body>");
+        out.print("<p>Postback received</p>");
+        out.print("</body></html>");
+    }
+
+}
