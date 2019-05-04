e1e722ff6f714c62bc235c0571be4397228e6d2f
phuongtm1911 <50118891+phuongtm1911@users.noreply.github.com>
2019-05-04 02:35:50 -0500

    Update SimpleCalculator2.java

--- SimpleCalculatorLab/SimpleCalculator2.java
+++ SimpleCalculatorLab/SimpleCalculator2.java
@@ -19,48 +19,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "SimpleCalculator2", urlPatterns = {"/SimpleCalculator2"})
 public class SimpleCalculator2 extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SimpleCalculator2</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SimpleCalculator2 at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -74,7 +32,7 @@ public class SimpleCalculator2 extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        response.sendRedirect("http://localhost:8080/SimpleCalculatorLab");
+        RequestDispatcher view = request.getRequestDispatcher("index.jsp");  
         String f21 = request.getParameter("field21");
         String f22 = request.getParameter("field22");
         String f23 = request.getParameter("field23");
@@ -83,24 +41,15 @@ public class SimpleCalculator2 extends HttpServlet {
             int n21 = Integer.parseInt(f21);
             int n22 = Integer.parseInt(f22);
             int s = n21 + n22;
-            request.setAttribute("value", s);
+            request.setAttribute("sum", s);
         }
         if (f23 != null && f24 != null && !f23.equals("") && !f24.equals("")) {
             int n23 = Integer.parseInt(f23);
             int n24 = Integer.parseInt(f24);
             int m = n23 * n24;
-            request.setAttribute("value", m);
+            request.setAttribute("multiplication", m);
         }
+        view.forward(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
