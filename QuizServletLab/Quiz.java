4f845757f7efc8751fe736851dd848f6ac834626
phuongtm1911 <50118891+phuongtm1911@users.noreply.github.com>
2019-05-07 21:55:25 -0500

    Update Quiz.java

--- QuizServletLab/Quiz.java
+++ QuizServletLab/Quiz.java
@@ -1,5 +1,3 @@
-import javax.servlet.http.*;
-
 public class Quiz {
     private static String[] questions = {
         "3, 1, 4, 1, 5", // pi
@@ -12,42 +10,34 @@ public class Quiz {
     private static int[] answers = {9, 8, 36, 13, 32};
     
     int score;
+    int currentQuestion;
     String question;
     
     public Quiz() {
         score = 0;
         question = questions[0];
+        currentQuestion = 0;
     }
     
-    public static String getQuestion(HttpSession pres) {
-        if (pres == null) {
-            return questions[0];
-        }
-        String preq = (String) pres.getAttribute("question");
-        for (int i = 0; i < questions.length - 1; i++) {
-            if (preq.equals(questions[i])) {
-                return questions[i + 1];
-            }
-        }
-        return null;
+    public String getQuestion() {
+        return questions[currentQuestion];
     }
-      
-    public static int getPreScore(HttpSession pres) {
-        if (pres == null) {
-            return 0;
-        } else {
-            return (int) pres.getAttribute("score");
+    
+    public int getScore(){
+        return score;
+    }    
+    
+    public void calculateScore(int answer) {
+        if (answers[currentQuestion] ==  answer){
+            score++;
+        } 
+        if (currentQuestion <= questions.length - 1) {
+            currentQuestion++;
         }
     }
-
-    public static int calculateScore(HttpSession pres, int answer) {
-        int preScore = 0;
-        for (int i = 0; i < 5; i++) {
-            if (getQuestion(pres).equals(questions[i]) && answer == answers[i]) {
-                preScore = getPreScore(pres);
-                return preScore++;
-            }
-        }
-        return getPreScore(pres); 
+       
+    public boolean quizOver(){
+        return currentQuestion > questions.length - 1;
     }
+    
 }
